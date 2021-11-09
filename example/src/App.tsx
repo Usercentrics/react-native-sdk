527d4df289b42bce9cd06fc2f727ad90c0137555
Pedro Araujo <90838270+uc-pedro@users.noreply.github.com>
2021-11-09 10:48:41 +0000

    Fix hot reload (#16)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { CustomScreen, HomeScreen } from './screens';
-import Consent from './screens/Consent';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +15,7 @@ const App = () => {
         />
         <Stack.Screen
           name="CustomUI"
-          component={Consent} />
+          component={CustomScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
