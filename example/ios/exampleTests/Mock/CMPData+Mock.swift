566aa3e919bf6e769bf5ef27b6d22ecb80bbe05c
Francisco Cunha <francisco.cunha@usercentrics.com>
2024-07-26 14:54:51 +0100

    starting to fix for ios

--- example/ios/exampleTests/Mock/CMPData+Mock.swift
+++ example/ios/exampleTests/Mock/CMPData+Mock.swift
@@ -181,6 +181,7 @@ extension UsercentricsLabels {
                  headerModal: "headerModal",
                  secondLayerDescriptionHtml: "secondLayerDescriptionHtml",
                  secondLayerTitle: "secondLayerTitle",
+                 
                  settings: "settings",
                  subConsents: "subConsents",
                  btnAccept: "btnAccept",
@@ -201,6 +202,7 @@ extension UsercentricsLabels {
                  cookiePolicyLinkText: "cookiePolicyLinkText",
                  noImplicit: "noImplicit",
                  yesImplicit: "yesImplicit",
+                 
                  addressOfProcessingCompany: "addressOfProcessingCompany",
                  consentType: "consentType",
                  consents: "consents",
