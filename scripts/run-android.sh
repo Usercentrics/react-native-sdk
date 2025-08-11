367539d96dfb24501b21e3c7c64d1a00c1a5d0e6
uc-brunosouza <bruno.souza@usercentrics-ext.com>
2025-08-11 16:31:41 -0300

    chore: remove unnecessary parameter at script

--- scripts/run-android.sh
+++ scripts/run-android.sh
@@ -5,4 +5,4 @@ lsof -i tcp:7007 | awk 'NR!=1 {print $2}' | xargs kill
 # adb reverse tcp:8081 tcp:8081
 # adb reverse tcp:7007 tcp:7007
 # adb reverse tcp:8097 tcp:8097
-react-native run-android --no-packager --variant $VARIANT
+react-native run-android --no-packager
