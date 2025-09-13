3e7405ba989f3dba836e0d548080ae5378d50aa4
uc-brunosouza <bruno.souza@usercentrics-ext.com>
2025-09-13 13:15:59 -0300

    chore: add integration with usercentrics sdk

--- sample/.eslintrc.js
+++ sample/.eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
+  extends: ['@react-native-community', 'plugin:prettier/recommended'],
+  plugins: ['simple-import-sort'],
   root: true,
-  extends: '@react-native',
-};
+  rules: {
+    'import/order': 'off',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-imports': 'off',
+  },
+}
--- sample/android/app/src/main/java/com/usercentrics/reactnativesdk/sample/MainApplication.kt
+++ sample/android/app/src/main/java/com/usercentrics/reactnativesdk/sample/MainApplication.kt
@@ -1,20 +1,17 @@
 package com.usercentrics.reactnativesdk.sample
 
 import android.app.Application
-import com.facebook.react.PackageList
-import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import android.content.Context
+import com.facebook.react.*
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import java.lang.reflect.InvocationTargetException
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
+  override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
@@ -36,8 +33,44 @@ class MainApplication : Application(), ReactApplication {
     super.onCreate()
     SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+      DefaultNewArchitectureEntryPoint.load(
+        true,
+        true,
+        false
+      )
+    }
+
+    if (BuildConfig.DEBUG) {
+      initializeFlipper(this, reactNativeHost.reactInstanceManager)
+    }
+  }
+
+  companion object {
+
+    private fun initializeFlipper(
+      context: Context,
+      reactInstanceManager: ReactInstanceManager
+    ) {
+      if (BuildConfig.DEBUG) {
+        try {
+          val aClass = Class.forName("com.usercentrics.reactnativesdk.sample.ReactNativeFlipper")
+          aClass
+            .getMethod(
+              "initializeFlipper",
+              Context::class.java,
+              ReactInstanceManager::class.java
+            )
+            .invoke(null, context, reactInstanceManager)
+        } catch (e: ClassNotFoundException) {
+          e.printStackTrace()
+        } catch (e: NoSuchMethodException) {
+          e.printStackTrace()
+        } catch (e: IllegalAccessException) {
+          e.printStackTrace()
+        } catch (e: InvocationTargetException) {
+          e.printStackTrace()
+        }
+      }
     }
   }
 }
