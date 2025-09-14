a0ac6ec3797f81495e945764f3afc726e9d27fdb
uc-brunosouza <bruno.souza@usercentrics-ext.com>
2025-09-14 11:22:01 -0300

    chore: improve android configs

--- sample/android/build.gradle.kts
+++ sample/android/build.gradle.kts
@@ -1,20 +1,12 @@
 buildscript {
-    extra.apply {
-        set("buildToolsVersion", "34.0.0")
-        set("minSdkVersion", 23)
-        set("compileSdkVersion", 34)
-        set("targetSdkVersion", 34)
-        set("ndkVersion", "26.1.10909125")
-        set("kotlinVersion", "1.9.22")
-    }
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle")
-        classpath("com.facebook.react:react-native-gradle-plugin")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin")
+        classpath(libs.gradle.plugin)
+        classpath(libs.react.native.plugin)
+        classpath(libs.kotlin.plugin)
     }
 }
 
--- sample/android/settings.gradle.kts
+++ sample/android/settings.gradle.kts
@@ -1,5 +1,13 @@
 rootProject.name = "sample"
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
 apply(from = file("../node_modules/@react-native-community/cli-platform-android/native_modules.gradle"))
 val applyNativeModulesSettingsGradle: groovy.lang.Closure<Any> by extra
 applyNativeModulesSettingsGradle(settings)
