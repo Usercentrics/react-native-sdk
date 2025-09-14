fdc6ee4be39ab77c54deee028d6c09e6d88954ce
Bruno Souza <souza.brunoj@gmail.com>
2025-09-14 10:57:55 -0300

    fix: git files

---
+++ sample/android/settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "sample"
+
+apply(from = file("../node_modules/@react-native-community/cli-platform-android/native_modules.gradle"))
+val applyNativeModulesSettingsGradle: groovy.lang.Closure<Any> by extra
+applyNativeModulesSettingsGradle(settings)
+
+include(":app")
+includeBuild("../node_modules/@react-native/gradle-plugin")
